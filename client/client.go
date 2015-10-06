380affebd9018d60c5f182e007a0dc7ffee1e491
Gaetan Nicod <nicod.ga@gmail.com>
2015-10-06 23:49:30 +0200

    replace unix socket by tpc tls

--- client/client.go
+++ client/client.go
@@ -1,63 +1,78 @@
 package client
 
 import (
-    "bytes"
-    "net"
-    //"os"
-    "log"
-    "time"
-    "encoding/gob"
-    "github.com/gnicod/aion/scheduler"
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"log"
+	"net"
+	//"os"
+	"encoding/gob"
+	"github.com/gnicod/aion/scheduler"
+	"time"
 )
 
-type Client struct{
-    conn net.Conn
+type Client struct {
+	conn net.Conn
 }
 
-func NewClient() Client{
-    conn, err := net.Dial("unix", "/tmp/aion.sock")
-    if err != nil {
-        panic(err)
-    }
-    client := Client{conn}
-    go client.reader()
-    return client
+func NewClient() Client {
+	cert, err := tls.LoadX509KeyPair("/home/ovski/certs/client.pem", "/home/ovski/certs/client.key")
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", "127.0.0.1:8000", &config)
+	if err != nil {
+		log.Fatalf("client: dial: %s", err)
+	}
+	defer conn.Close()
+	log.Println("client: connected to: ", conn.RemoteAddr())
+
+	state := conn.ConnectionState()
+	for _, v := range state.PeerCertificates {
+		fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
+		fmt.Println(v.Subject)
+	}
+	log.Println("client: handshake: ", state.HandshakeComplete)
+	log.Println("client: mutual: ", state.NegotiatedProtocolIsMutual)
+	client := Client{conn}
+	go client.reader()
+	return client
 }
 
-func (c *Client) reader(){
-    buf := make([]byte, 1024)
-    for {
-        n, err := c.conn.Read(buf[:])
-        defer c.conn.Close()
-        if err != nil {
-            return
-        }
-        println("Client got:", string(buf[0:n]))
-        //oO violent
-    }
+func (c *Client) reader() {
+	reply := make([]byte, 256)
+	n, err := c.conn.Read(reply)
+	if err != nil {
+		log.Fatal("write error:", err)
+	}
+	log.Printf("client: got %q (%d bytes)", string(reply[:n]), n)
+	log.Print("client: exiting")
 }
 
-func (c *Client) AddTask(t scheduler.Task){
-    var network bytes.Buffer
-    enc := gob.NewEncoder(&network)
-    err := enc.Encode(t)
-    if err != nil {
-        log.Fatal("encode error:", err)
-    }
-    _, err = c.conn.Write(network.Bytes())
-    if err != nil {
-        log.Fatal("write error:", err)
-    }
+func (c *Client) AddTask(t scheduler.Task) {
+	var network bytes.Buffer
+	enc := gob.NewEncoder(&network)
+	err := enc.Encode(t)
+	if err != nil {
+		log.Fatal("encode error:", err)
+	}
+	_, err = c.conn.Write(network.Bytes())
+	if err != nil {
+		log.Fatal("write error:", err)
+	}
 }
 
-func (c *Client) Send(){
-    defer c.conn.Close()
-    for {
-        _, err := c.conn.Write([]byte("hi"))
-        if err != nil {
-            log.Fatal("write error:", err)
-            break
-        }
-        time.Sleep(10e9)
-    }
+func (c *Client) Send() {
+	defer c.conn.Close()
+	for {
+		_, err := c.conn.Write([]byte("hi"))
+		if err != nil {
+			log.Fatal("write error:", err)
+			break
+		}
+		time.Sleep(10e9)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,47 +1,69 @@
 package server
 
 import (
-    "log"
-    "net"
-    "encoding/gob"
-    "github.com/gnicod/aion/scheduler"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/gob"
+	"github.com/gnicod/aion/scheduler"
+	"log"
+	"net"
 )
 
 type Server struct {
-    l net.Listener
-    sch scheduler.Scheduler
+	l   net.Listener
+	sch scheduler.Scheduler
 }
 
-func NewServer(sch scheduler.Scheduler) Server{
-    l, err := net.Listen("unix","/tmp/aion.sock" )
-    if err != nil {
-        log.Fatal("listen error:", err)
-    }
-    serv := Server{l, sch}
-    return serv
+func NewServer(sch scheduler.Scheduler) Server {
+	// TODO path need to be read from a config
+	cert, err := tls.LoadX509KeyPair("/home/ovski/certs/server.pem", "/home/ovski/certs/server.key")
+	if err != nil {
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}}
+	config.Rand = rand.Reader
+	service := "0.0.0.0:8000"
+	l, err := tls.Listen("tcp", service, &config)
+	if err != nil {
+		log.Fatalf("server: listen: %s", err)
+	}
+	log.Print("server: listening")
+	serv := Server{l, sch}
+	return serv
 }
 
-
-func (s *Server) Listen(){
-    for {
-        fd, err := s.l.Accept()
-        if err != nil {
-            log.Fatal("accept error:", err)
-        }
-        go s.serve(fd)
-    }
+func (s *Server) Listen() {
+	for {
+		conn, err := s.l.Accept()
+		if err != nil {
+			log.Printf("server: accept: %s", err)
+			break
+		}
+		defer conn.Close()
+		log.Printf("server: accepted from %s", conn.RemoteAddr())
+		tlscon, ok := conn.(*tls.Conn)
+		if ok {
+			log.Print("ok=true")
+			state := tlscon.ConnectionState()
+			for _, v := range state.PeerCertificates {
+				log.Print(x509.MarshalPKIXPublicKey(v.PublicKey))
+			}
+		}
+		go s.serve(conn)
+	}
 }
 
-func (s *Server) serve(c net.Conn){
-        dec := gob.NewDecoder(c)
-        t := &scheduler.Task{}
-        dec.Decode(t)
-        println("Received : %+v", t);
+func (s *Server) serve(c net.Conn) {
+	dec := gob.NewDecoder(c)
+	t := &scheduler.Task{}
+	dec.Decode(t)
+	println("Received : %+v", t)
 
-        println("Command:", string(t.Command))
-        println("Expression:", string(t.Expression))
-        _, err := c.Write([]byte(t.Command))
-        if err != nil {
-            log.Fatal("Write: ", err)
-        }
+	println("Command:", string(t.Command))
+	println("Expression:", string(t.Expression))
+	_, err := c.Write([]byte(t.Command))
+	if err != nil {
+		log.Fatal("Write: ", err)
+	}
 }
